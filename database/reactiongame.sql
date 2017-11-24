c2d71070ab3357fe9911c90daeb439c4bd451487
DyumanBulloni <dyuman.bulloni@samtrevano.ch>
2017-11-24 15:18:56 +0100

    Aggiunta prima versione database

---
+++ database/reactiongame.sql
@@ -0,0 +1,19 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              10.1.22-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win32
+-- HeidiSQL Versione:            9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- L’esportazione dei dati non era selezionata.
+-- L’esportazione dei dati non era selezionata.
+-- L’esportazione dei dati non era selezionata.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
