62131fe8ede0ded4825ea644a1656cf2ca65cf13
DyumanBulloni <dyuman.bulloni@samtrevano.ch>
2017-12-22 14:33:23 +0100

    Aggiunte modalità 8,19

---
+++ codice/mod19/mod19.ino
@@ -0,0 +1,122 @@
+/**
+ * Array contenente il vecchio stato dei bottoni. 
+ */
+boolean lastButtonsState[4];
+
+/**
+ * Array contenente lo stato attuale dei bottoni.
+ */
+boolean currentButtonsState[4];
+
+/**
+ * Booleano che verifica se si è presenti nel primo ciclo.
+ */
+boolean firstCicle = true;
+
+/**
+ * Array contenente i pins dei bottoni.
+ */
+int buttonPins[] = {1,2,3,4};
+
+/**
+ * Array contenente i pins dei led
+ */
+int ledPins[] = {5,6,7,8};
+
+/**
+ * Il numero di millisecondi mancanti alla fine del gioco.
+ */
+int countdown = 18000;
+
+/**
+ * Valore relativo al vecchio indice del led da accendere.
+ */
+int lastNumber = -1;
+
+/**
+ * Valore relativo all'indice attuale del led da accendere.
+ */
+int currentNumber = -1;
+
+/**
+ * Punteggio della partita
+ */
+int score = 0;
+
+void setup() 
+{
+  for(int i = 0; i < sizeof(buttonPins); i++)
+  {
+    //Assegno i pin ai relativi bottoni e led.
+    pinMode(buttonPins[i], INPUT);
+    pinMode(ledPins[i], OUTPUT);
+
+    //Riempio di false i due array.
+    lastButtonsState[i] = false;
+    currentButtonsState[i] = false;
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{ 
+  if(firstCicle)
+  {
+    lightLed();
+    firstCicle = false;  
+  }
+  
+  for(int i = 0; i < sizeof(buttonPins); i++)
+  {
+    currentButtonsState[i] = debounce(i); 
+    //digitalRead(buttonPins[currentNumber]);
+
+    if(currentButtonsState[i] == true && currentButtonsState[i] != lastButtonsState[i] && i == currentNumber)
+    {
+      score++;
+      lightLed();
+    }
+    
+    lastButtonsState[i] = currentButtonsState[i];
+    countdown--;
+    delay(1);
+    
+    if(countdown == 0)
+    {
+      exit(0);
+    }
+  } 
+}
+
+boolean debounce(int n)
+{
+   boolean current = digitalRead(buttonPins[n]); 
+   if(lastButtonsState[n] != current)
+   {
+       delay(5);
+       current = digitalRead(buttonPins[n]);
+   }
+   return current;
+}
+
+void lightLed()
+{
+  boolean a = true;
+  while(a)
+  {
+    currentNumber = random(0,9); 
+    
+    if(currentNumber != lastNumber)
+    {
+      a = false;
+    }
+  }
+  lastNumber = currentNumber;
+
+  for(int i = 0; i < sizeof(ledPins); i++)
+  {
+    digitalWrite(ledPins[i], LOW);
+  }
+  digitalWrite(ledPins[currentNumber], HIGH);
+}
---
+++ codice/mod8/mod8.ino
@@ -0,0 +1,133 @@
+/**
+ * Array contenente il vecchio stato dei bottoni. 
+ */
+boolean lastButtonsState[12];
+
+/**
+ * Array contenente lo stato attuale dei bottoni.
+ */
+boolean currentButtonsState[10];
+
+/**
+ * Booleano che verifica se si è presenti nel primo ciclo.
+ */
+boolean firstCicle = true;
+
+/**
+ * Array contenente i pins dei bottoni.
+ */
+int buttonPins[] = {1,2,3,4,5,6,7,8,9,10};
+
+/**
+ * Array contenente i pins dei led
+ */
+int ledPins[] = {11,12,13,14,15,16,17,18,19,20};
+
+/**
+ * Il numero di millisecondi mancanti alla fine del gioco.
+ */
+int countdown = 30000;
+
+int countdownLoop = 0;
+
+/**
+ * Valore relativo al vecchio indice del led da accendere.
+ */
+int lastNumber = -1;
+
+/**
+ * Valore relativo all'indice attuale del led da accendere.
+ */
+int currentNumber = -1;
+
+/**
+ * Punteggio della partita
+ */
+int score = 0;
+
+void setup() 
+{
+  for(int i = 0; i < sizeof(buttonPins)/sizeof(buttonPins[0]); i++)
+  {
+    //Assegno i pin ai relativi bottoni e led.
+    pinMode(buttonPins[i], INPUT);
+    pinMode(ledPins[i], OUTPUT);
+
+    //Riempio di false i due array.
+    lastButtonsState[i] = false;
+    currentButtonsState[i] = false;
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{ 
+  if(firstCicle)
+  {
+    lightLed();
+    firstCicle = false;  
+  }
+  
+  for(int i = 0; i < sizeof(buttonPins) / sizeof(buttonPins[0]); i++)
+  {
+    currentButtonsState[i] = debounce(i); 
+    //digitalRead(buttonPins[currentNumber]);
+
+    if(currentButtonsState[i] == true && currentButtonsState[i] != lastButtonsState[i] && i == currentNumber)
+    {
+      score++;
+      lightLed();
+    }
+    
+    lastButtonsState[i] = currentButtonsState[i];
+    countdown--;
+    delay(1);
+    
+    if(countdown == 0)
+    {
+      countdownLoop++;
+
+      if(countdownLoop == 4)
+      {
+        exit(0);
+      }
+      else
+      {
+        countdown = 30000;
+      }
+    }
+  } 
+}
+
+boolean debounce(int n)
+{
+   boolean current = digitalRead(buttonPins[n]); 
+   if(lastButtonsState[n] != current)
+   {
+       delay(5);
+       current = digitalRead(buttonPins[n]);
+   }
+   return current;
+}
+
+void lightLed()
+{
+  boolean a = true;
+  while(a)
+  {
+    currentNumber = random(0,9); 
+    
+    if(currentNumber != lastNumber)
+    {
+      a = false;
+    }
+  }
+  lastNumber = currentNumber;
+
+  for(int i = 0; i < sizeof(ledPins) / sizeof(ledPins[0]); i++)
+  {
+    digitalWrite(ledPins[i], LOW);
+  }
+  digitalWrite(ledPins[currentNumber], HIGH);
+}
