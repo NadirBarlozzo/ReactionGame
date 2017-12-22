a10986da9ba4b3fcd3f30a430903f424750acc87
DyumanBulloni <dyuman.bulloni@samtrevano.ch>
2017-12-22 13:52:21 +0100

    Modificata modalità 7

--- codice/mod7/mod7.ino
+++ codice/mod7/mod7.ino
@@ -1,7 +1,7 @@
-int buttonPins[] = {1,2,3,4};
-int ledPins[] = {5,6,7,8};
-boolean lastButtonsState[4];
-boolean currentButtonsState[4];
+int buttonPins[] = {1,2,3,4,5,6,7,8,9,10};
+int ledPins[] = {11,12,13,14,15,16,17,18,19,20};
+boolean lastButtonsState[10];
+boolean currentButtonsState[10];
 boolean pressed = false;
 long startTime;
 long endTime;
